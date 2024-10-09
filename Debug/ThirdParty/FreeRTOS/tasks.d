b91906dbcd130e2633554c79764593422ddc7d90
Paul Beschiu <paulbeschiu8@gmail.com>
2024-10-09 17:32:04 +0300

    Git init

---
+++ Debug/ThirdParty/FreeRTOS/tasks.d
@@ -0,0 +1,41 @@
+ThirdParty/FreeRTOS/tasks.o: ../ThirdParty/FreeRTOS/tasks.c \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/FreeRTOSConfig.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/SEGGER/SEGGER.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/Config/Global.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Conf.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/list.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/timers.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/stack_macros.h
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/FreeRTOSConfig.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/SEGGER/SEGGER.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/Config/Global.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Conf.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/portable.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/list.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/timers.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/stack_macros.h:
---
+++ Debug/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Config_FreeRTOS.d
@@ -0,0 +1,32 @@
+ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Config_FreeRTOS.o: \
+ ../ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Config_FreeRTOS.c \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/FreeRTOSConfig.h \
+ ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/SEGGER/SEGGER.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/Config/Global.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Conf.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/mpu_wrappers.h
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/FreeRTOSConfig.h:
+../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/OS/SEGGER_SYSVIEW_FreeRTOS.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/SEGGER/SEGGER.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/Config/Global.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/Config/SEGGER_SYSVIEW_Conf.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/portable.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/paulb/OneDrive/Desktop/Utilities/StmCubeWorkplace/LearningFreeRTOS/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
